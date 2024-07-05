97524d4e29c6ae3fd44e10b258ae01b8dbf1fa04
YANNICK LERNER <47801170@ort>
2024-07-05 10:30:21 -0300

    agregue ico

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="icon" href="/dante.png"/>
+<title>aieka</title>
       <Head />
       <body>
         <Main />
